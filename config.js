f885912649896aa77d15d3d5ee086dbf0360c3f1
Karl Denby <karl.denby@gmail.com>
2019-08-26 14:30:28 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    username: "test-user",
+    password: "test-password",
+    jwt: "test-jwt",
+    address: "my-instance-name.region.cloud.realm.io",
+    path: "/~/test-realm"
+};
